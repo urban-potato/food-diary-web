f739d2d3f8feaa3270221c2b9533f3f599e35ce0
Urban Potato <veryprogramming@gmail.com>
2024-05-13 01:07:53 +0700

    Small changes

--- src/modules/DiaryModule/components/DaysCharacteristicsSum.tsx
+++ src/modules/DiaryModule/components/DaysCharacteristicsSum.tsx
@@ -10,7 +10,7 @@ const DaysCharacteristicsSum: FC<TProps> = ({ characteristicsSum }) => {
   return (
     <div
       className="bg-gradient-to-r from-pink-200 to-violet-200 shadow-lg rounded-xl p-5 
-      flex flex-col flex-wrap justify-center items-start gap-3 max-w-max"
+      flex flex-col flex-wrap justify-center items-start gap-3 w-full"
     >
       {/* {characteristicsSum.length > 0 ? (
         <CharacteristicsSumList characteristicsSum={characteristicsSum} />
