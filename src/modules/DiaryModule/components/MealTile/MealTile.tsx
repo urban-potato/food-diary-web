c8450e5f73e0d6012f89e9e64978b37fff166e3f
Urban Potato <veryprogramming@gmail.com>
2024-05-15 13:22:28 +0700

    Changes in the Design

--- src/modules/DiaryModule/components/MealTile/MealTile.tsx
+++ src/modules/DiaryModule/components/MealTile/MealTile.tsx
@@ -1,13 +1,9 @@
-import { FC, useRef, useState } from "react";
-import { Player } from "@lordicon/react";
-import EDIT_ICON from "../../../../global/assets/system-regular-63-settings-cog.json";
-import DELETE_ICON from "../../../../global/assets/system-regular-39-trash.json";
+import { FC, useState } from "react";
 import { useDeleteCourseMealMutation } from "../../api/meals.api";
 import MealEditForm from "./MealEditForm";
-import ConsumedDishesList from "../ConsumedDishesList/ConsumedDishesList";
 import { ICourseMeal } from "../../../../global/types/types";
-import CharacteristicTilesList from "../../../../components/CharacteristicTilesList/CharacteristicTilesList";
 import MealInfo from "./MealInfo";
+import TileIcons from "../../../../components/TileIcons/TileIcons";
 
 const MealTile: FC<ICourseMeal> = ({
   id,
@@ -20,57 +16,20 @@ const MealTile: FC<ICourseMeal> = ({
 }) => {
   const [isEditMode, setIsEditMode] = useState(false);
   const [doDeleteCourseMeal] = useDeleteCourseMealMutation();
-  const deleteMeal = () => {
-    doDeleteCourseMeal(id).catch((e: any) => console.log(e));
+  const deleteMeal = async () => {
+    await doDeleteCourseMeal(id).catch((e: any) => console.log(e));
   };
-
-  const editIconPlayerRef = useRef<Player>(null);
-  const deleteIconPlayerRef = useRef<Player>(null);
-  const ICON_SIZE = 28;
 
   return (
     <div className="outer_box_style group w-full max-w-5xl mt-5">
-      <div className="box_style "></div>
-      <div className="box_content_transition flex flex-col flex-wrap w-full justify-center items-start pl-7 pr-6 py-7">
-        <div className="w-full max-w-5xl  flex justify-center items-center ">
-          <div className="ml-1 flex gap-5 justify-center items-center self-center ">
-            <div className="text-xl font-bold">
-              {creationTime.split(".")[0].split(":").slice(0, 2).join(":")}
-            </div>
-            <div className="text-xl font-bold">{mealTypeName}</div>
-          </div>
-
-          <div className="mr-0 mx-auto justify-self-end gap-x-2 flex justify-center items-start ">
-            <span role="button" onClick={() => setIsEditMode(!isEditMode)}>
-              <span
-                onMouseEnter={() =>
-                  editIconPlayerRef.current?.playFromBeginning()
-                }
-              >
-                <Player
-                  ref={editIconPlayerRef}
-                  icon={EDIT_ICON}
-                  size={ICON_SIZE}
-                  colorize="#0d0b26"
-                />
-              </span>
-            </span>
-
-            <span role="button" onClick={() => deleteMeal()}>
-              <span
-                onMouseEnter={() =>
-                  deleteIconPlayerRef.current?.playFromBeginning()
-                }
-              >
-                <Player
-                  ref={deleteIconPlayerRef}
-                  icon={DELETE_ICON}
-                  size={ICON_SIZE}
-                  colorize="#0d0b26"
-                />
-              </span>
-            </span>
-          </div>
+      <div className="box_style"></div>
+      <div className="box_content_transition flex flex-col flex-wrap w-full justify-center items-start p-7">
+        <div className="box_icons">
+          <TileIcons
+            isEditMode={isEditMode}
+            setIsEditMode={setIsEditMode}
+            handleDelete={deleteMeal}
+          />
         </div>
 
         {isEditMode ? (
@@ -83,6 +42,8 @@ const MealTile: FC<ICourseMeal> = ({
           />
         ) : (
           <MealInfo
+            creationTime={creationTime}
+            mealTypeName={mealTypeName}
             consumedElementaries={consumedElementaries}
             consumedRecipes={consumedRecipes}
             characteristicsSum={characteristicsSum}
