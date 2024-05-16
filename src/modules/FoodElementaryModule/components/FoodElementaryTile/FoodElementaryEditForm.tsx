5f7fa7312d565bb9ccbcb18e1ca692f4e999c911
Urban Potato <veryprogramming@gmail.com>
2024-05-16 21:49:04 +0700

    Refactoring: Food elementary Edit Form

--- src/modules/FoodElementaryModule/components/FoodElementaryTile/FoodElementaryEditForm.tsx
+++ src/modules/FoodElementaryModule/components/FoodElementaryTile/FoodElementaryEditForm.tsx
@@ -31,6 +31,8 @@ import {
   SELECT_STYLES,
 } from "../../../../global/constants/constants.ts";
 import NoOptionsMessage from "../../../../components/NoOptionsMessage/NoOptionsMessage.tsx";
+import DisabledSelectRowWithWeightField from "../../../../components/DisabledSelectRowWithWeightField/DisabledSelectRowWithWeightField.tsx";
+import AsyncSelectRowWithWeightField from "../../../../components/AsyncSelectRowWithWeightField/AsyncSelectRowWithWeightField.tsx";
 
 type TProps = {
   foodElementaryId: string;
@@ -416,7 +418,7 @@ const FoodElementaryEditForm: FC<TProps> = ({
   };
 
   return (
-    <div className="w-full max-w-5xl flex flex-col justify-center items-start -mt-12">
+    <div className="w-full max-w-5xl flex flex-col justify-center items-start -mt-5">
       <form
         className="flex flex-col flex-wrap justify-center w-full"
         onSubmit={handleSubmit(onSubmit)}
@@ -433,6 +435,7 @@ const FoodElementaryEditForm: FC<TProps> = ({
             }}
             isRequired={true}
             addSpaceAfterLabel={true}
+            labelSize={"text-lg"}
           />
         </div>
 
@@ -454,7 +457,7 @@ const FoodElementaryEditForm: FC<TProps> = ({
           </div>
         )}
 
-        <div className="text-xl w-full flex-grow">
+        <div className="text-xl w-full flex-grow mt-2 mb-5">
           <InputIlluminated
             id={"FoodElementaryEditForm_caloriesValue"}
             type="number"
@@ -465,6 +468,7 @@ const FoodElementaryEditForm: FC<TProps> = ({
               ...register("caloriesValue"),
             }}
             isRequired={true}
+            labelSize={"text-lg"}
           />
         </div>
         {errors.caloriesValue && (
@@ -482,239 +486,74 @@ const FoodElementaryEditForm: FC<TProps> = ({
         )}
 
         <div className="flex flex-col">
-          <h3 className="text-xl my-3">Нутриенты:</h3>
-
-          {originalCharacteristicsFields.map((select, index) => {
+          {originalCharacteristicsFields.map((item, index) => {
             return (
-              <div
-                key={`FoodElementaryEditForm_div_originalCharacteristicsFields_${select.id}_${index}`}
-                className="form-control flex flex-col"
-              >
-                <div className="gap-x-3 flex mb-1">
-                  <div className="flex flex-col justify-center gap-3 flex-grow mb-3">
-                    <span className="flex gap-x-1">
-                      <h3>Нутриент</h3>
-                      <p className="text-red">*</p>
-                    </span>
-                    <Controller
-                      key={`FoodElementaryEditForm_controller_originalCharacteristicsFields_${select.id}_${index}`}
-                      name={
-                        `originalCharacteristicsList.${index}.characteristicInfo` as const
-                      }
-                      control={control}
-                      render={({ field }) => (
-                        <Select
-                          {...field}
-                          key={`FoodElementaryEditForm_select_originalCharacteristicsFields_${select.id}_${index}`}
-                          className="relative text-sm rounded-xl  "
-                          styles={SELECT_STYLES}
-                          isDisabled={true}
-                        />
-                      )}
-                    />
-                  </div>
-
-                  <div className="-mt-4 sm:max-w-[100px] max-w-[80px] flex-grow">
-                    <InputIlluminated
-                      id={`originalCharacteristicsList.${index}.characteristicValue`}
-                      type="number"
-                      placeholder="Вес (г)"
-                      disableIllumination={true}
-                      additionalStyles=" h-[67px] border-0 "
-                      register={{
-                        ...register(
-                          `originalCharacteristicsList.${index}.characteristicValue` as const
-                        ),
-                      }}
-                      isRequired={true}
-                    />
-                  </div>
-
-                  <div className="max-w-[60px] flex flex-col justify-center items-center">
-                    <h3 className="text-lg my-3"> </h3>
-                    <ButtonIlluminated
-                      label={
-                        <span>
-                          <Player
-                            ref={deleteIconPlayerRef}
-                            icon={DELETE_ICON}
-                            size={ICON_SIZE}
-                            colorize="#f8f7f4"
-                          />
-                        </span>
-                      }
-                      isDarkButton={true}
-                      isIlluminationFull={false}
-                      onClick={() => {
-                        handleRemoveOriginalCharacteristic(index);
-                      }}
-                      buttonPadding=" p-[14px] "
-                      additionalStyles=" "
-                      isDisabled={index < 3 ? true : false}
-                    />
-                  </div>
-                </div>
-
-                {errors.originalCharacteristicsList && (
-                  <div
-                    className={
-                      Object.keys(errors).length > 0 &&
-                      errors.originalCharacteristicsList[index]
-                        ? "flex flex-col mb-2 px-5 gap-y-2 justify-center"
-                        : "hidden"
-                    }
-                  >
-                    <p
-                      className={
-                        errors.originalCharacteristicsList[index]
-                          ?.characteristicInfo?.value
-                          ? "text-pink-500 "
-                          : " hidden "
-                      }
-                    >
-                      {
-                        errors.originalCharacteristicsList[index]
-                          ?.characteristicInfo?.value?.message
-                      }
-                    </p>
-                    <p
-                      className={
-                        errors.originalCharacteristicsList[index]
-                          ?.characteristicValue
-                          ? "text-pink-500 "
-                          : " hidden "
-                      }
-                    >
-                      {
-                        errors.originalCharacteristicsList[index]
-                          ?.characteristicValue?.message
-                      }
-                    </p>
-                  </div>
-                )}
-              </div>
+              <DisabledSelectRowWithWeightField
+                key={`FoodElementaryEditForm_div_originalCharacteristicsFields_${item.id}_${index}`}
+                itemId={item.id}
+                itemIndex={index}
+                label={"Нутриент"}
+                selectPlaceholder={"Введите название нутриента"}
+                handleRemoveItem={handleRemoveOriginalCharacteristic}
+                controllerName={
+                  `originalCharacteristicsList.${index}.characteristicInfo` as const
+                }
+                control={control}
+                register={{
+                  ...register(
+                    `originalCharacteristicsList.${index}.characteristicValue` as const
+                  ),
+                }}
+                errors={errors}
+                errorsGroup={errors.originalCharacteristicsList}
+                errorSelect={
+                  errors.originalCharacteristicsList?.[index]
+                    ?.characteristicInfo?.value
+                }
+                errorFeild={
+                  errors.originalCharacteristicsList?.[index]
+                    ?.characteristicValue
+                }
+                isDeleteButtonDisabled={index < 3 ? true : false}
+              />
             );
           })}
 
-          {addCharacteristicListFields.map((select, index) => {
+          {addCharacteristicListFields.map((item, index) => {
             return (
-              <div
-                key={`FoodElementaryEditForm_Div_addCharacteristicsList_${select.id}_${index}`}
-                className="form-control flex flex-col"
-              >
-                <div className="gap-x-3 flex mb-1">
-                  <div className="flex flex-col justify-center gap-3 flex-grow mb-3">
-                    <span className="flex gap-x-1">
-                      <h3>Нутриент</h3>
-                      <p className="text-red">*</p>
-                    </span>
-                    <Controller
-                      key={`FoodElementaryEditForm_Controller_addCharacteristicsList_${select.id}_${index}`}
-                      name={
-                        `addCharacteristicsList.${index}.characteristicInfo` as const
-                      }
-                      control={control}
-                      render={({ field }) => (
-                        <AsyncSelect
-                          {...field}
-                          key={`FoodElementaryEditForm_AsyncSelect_addCharacteristicsList_${select.id}_${index}`}
-                          className="relative text-sm rounded-xl  "
-                          components={{ NoOptionsMessage }}
-                          styles={SELECT_STYLES}
-                          placeholder="Введите название нутриента"
-                          loadOptions={loadOptions}
-                          onInputChange={handleOnInputChange}
-                          onChange={(newValue) => {
-                            handleOnChange(newValue as TSelectElement, index);
-                            field.onChange(newValue);
-                          }}
-                        />
-                      )}
-                    />
-                  </div>
-
-                  <div className="-mt-4 sm:max-w-[100px] max-w-[80px] flex-grow">
-                    <InputIlluminated
-                      id={`FoodElementaryEditForm_addCharacteristicsList.${index}.characteristicValue`}
-                      type="number"
-                      placeholder="Вес (г)"
-                      disableIllumination={true}
-                      additionalStyles=" h-[67px] border-0 "
-                      register={{
-                        ...register(
-                          `addCharacteristicsList.${index}.characteristicValue` as const
-                        ),
-                      }}
-                      isRequired={true}
-                    />
-                  </div>
-
-                  <div className="max-w-[60px] flex flex-col justify-center items-center">
-                    <h3 className="text-lg my-3"> </h3>
-                    <ButtonIlluminated
-                      label={
-                        <span>
-                          <Player
-                            ref={deleteIconPlayerRef}
-                            icon={DELETE_ICON}
-                            size={ICON_SIZE}
-                            colorize="#f8f7f4"
-                          />
-                        </span>
-                      }
-                      isDarkButton={true}
-                      isIlluminationFull={false}
-                      onClick={() => {
-                        handleRemoveCharacteristicToAdd(index);
-                      }}
-                      buttonPadding=" p-[14px] "
-                      additionalStyles=" "
-                    />
-                  </div>
-                </div>
-
-                {errors.addCharacteristicsList && (
-                  <div
-                    className={
-                      Object.keys(errors).length > 0 &&
-                      errors.addCharacteristicsList[index]
-                        ? "flex flex-col mb-2 px-5 gap-y-2 justify-center"
-                        : "hidden"
-                    }
-                  >
-                    <p
-                      className={
-                        errors.addCharacteristicsList[index]?.characteristicInfo
-                          ?.value
-                          ? "text-pink-500 "
-                          : " hidden "
-                      }
-                    >
-                      {
-                        errors.addCharacteristicsList[index]?.characteristicInfo
-                          ?.value?.message
-                      }
-                    </p>
-                    <p
-                      className={
-                        errors.addCharacteristicsList[index]
-                          ?.characteristicValue
-                          ? "text-pink-500 "
-                          : " hidden "
-                      }
-                    >
-                      {
-                        errors.addCharacteristicsList[index]
-                          ?.characteristicValue?.message
-                      }
-                    </p>
-                  </div>
-                )}
-              </div>
+              <AsyncSelectRowWithWeightField
+                key={`FoodElementaryEditForm_Div_addCharacteristicsList_${item.id}_${index}`}
+                itemId={item.id}
+                itemIndex={index}
+                label={"Нутриент"}
+                selectPlaceholder={"Введите название нутриента"}
+                handleRemoveItem={handleRemoveCharacteristicToAdd}
+                controllerName={
+                  `addCharacteristicsList.${index}.characteristicInfo` as const
+                }
+                control={control}
+                register={{
+                  ...register(
+                    `addCharacteristicsList.${index}.characteristicValue` as const
+                  ),
+                }}
+                errors={errors}
+                errorsGroup={errors.addCharacteristicsList}
+                errorSelect={
+                  errors.addCharacteristicsList?.[index]?.characteristicInfo
+                    ?.value
+                }
+                errorFeild={
+                  errors.addCharacteristicsList?.[index]?.characteristicValue
+                }
+                loadSelectOptions={loadOptions}
+                handleOnSelectInputChange={handleOnInputChange}
+                handleOnSelectValueChange={handleOnChange}
+              />
             );
           })}
 
-          <div className="w-full max-w-[280px]">
+          <div className="w-full max-w-[280px] mt-3">
             <ButtonIlluminated
               label={"Добавить нутриент"}
               isDarkButton={true}
@@ -726,13 +565,13 @@ const FoodElementaryEditForm: FC<TProps> = ({
                   characteristicValue: 0,
                 });
               }}
-              buttonPadding=" p-[14px] "
+              buttonPadding=" p-[12px] "
               additionalStyles=""
             />
           </div>
         </div>
 
-        <div className="mt-7 flex flex-wrap w-full gap-x-4 gap-y-3 justify-stretch items-center">
+        <div className="mt-5 flex flex-wrap w-full gap-x-4 gap-y-3 justify-stretch items-center">
           <span className="flex-grow">
             <ButtonIlluminated
               label="Сохранить"
