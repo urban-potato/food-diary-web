a05b4f8453b356a6b8c84fe211a2b6e061740d27
Urban Potato <veryprogramming@gmail.com>
2024-05-26 19:46:05 +0700

    Change font

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,26 +5,16 @@ import Footer from "../components/Footer/Footer";
 
 const Layout: FC = () => {
   return (
-    <section
-      className=" min-h-screen h-full  flex flex-col 
-                text-near_black bg-near_white 
-                "
-    >
-      <section className=" h-max ">
+    <section className="min-h-screen h-full  flex flex-col text-near_black bg-near_white text-base">
+      <section className="h-max">
         <Header />
       </section>
 
-      <section
-        className=" h-full flex-grow 
-                    flex flex-col 
-                    justify-center items-center 
-                    py-5 px-5 
-                  "
-      >
+      <section className="h-full flex-grow flex flex-col justify-center items-center py-5 px-5">
         <Outlet />
       </section>
 
-      <section className=" h-max ">
+      <section className="h-max">
         <Footer />
       </section>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["17px", "26px"],
+      lg: ["18px", "28px"],
+      xl: ["20px", "28px"],
+      "2xl": ["24px", "32px"],
+      "3xl": ["30px", "36px"],
+      "4xl": ["36px", "40px"],
+      "5xl": ["48px", "1"],
+      "6xl": ["60px", "1"],
+      "7xl": ["72px", "1"],
+      "8xl": ["96px", "1"],
+      "9xl": ["128px", "1"],
+    },
     extend: {
       inset: {
         "01": "0.1px",
