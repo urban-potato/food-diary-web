369c64156dc9e53f8b093e3a0d9abdc82dfa9d9b
Urban Potato <veryprogramming@gmail.com>
2024-06-03 20:49:22 +0700

    Change toasts color

--- src/global/helpers/notify.helper.tsx
+++ src/global/helpers/notify.helper.tsx
@@ -21,11 +21,44 @@ export function notify({
   theme = "colored",
   toastId,
   toastType,
+  ...rest
 }: TProps) {
+  let backgroundColor;
+  let textColor;
+
+  switch (toastType) {
+    case "info":
+      backgroundColor = "#B149DA";
+      textColor = "#FFFFFF";
+      break;
+    case "success":
+      backgroundColor = "#06BC0B";
+      textColor = "#FFFFFF";
+      break;
+    case "warning":
+      backgroundColor = "#FF6644";
+      textColor = "#FFFFFF";
+      break;
+    case "error":
+      backgroundColor = "#EC0047";
+      textColor = "#FFFFFF";
+      break;
+    case "default":
+      backgroundColor = "#FFFFFF";
+      textColor = "#000000";
+      break;
+    default:
+      backgroundColor = "#FFFFFF";
+      textColor = "#000000";
+      break;
+  }
+
   toast(<ToastMessage messageText={messageText} />, {
+    ...rest,
     position: position,
     theme: theme,
     toastId: toastId,
     type: toastType,
+    style: { backgroundColor: backgroundColor, color: textColor },
   });
 }
