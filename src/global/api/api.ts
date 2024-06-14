58faddbd9955e12c236e64f795ddb6790b5c4c58
Urban Potato <veryprogramming@gmail.com>
2024-06-14 19:37:41 +0700

    Temp Ngrok url

--- src/global/api/api.ts
+++ src/global/api/api.ts
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getTokenFromLocalStorage } from "../helpers/local-storage.helper";
 
 const baseURL = "http://localhost:5000/";
+// const baseURL = "https://8054-193-218-138-73.ngrok-free.app";
 
 export const api = createApi({
   reducerPath: "api",
@@ -16,6 +17,8 @@ export const api = createApi({
     baseUrl: baseURL,
     prepareHeaders: (headers) => {
       const token = getTokenFromLocalStorage();
+
+      headers.set("ngrok-skip-browser-warning", "hehe");
 
       if (token) {
         headers.set("authorization", `Bearer ${token}`);
