fdb18c3befb5966b0bdd69f4d7b641e1e5b45b40
Urban Potato <veryprogramming@gmail.com>
2024-06-14 17:30:30 +0700

    Add link to meal page if meal not found

--- src/components/NoOptionsMessage/NoOptionsMessage.tsx
+++ src/components/NoOptionsMessage/NoOptionsMessage.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import { NoticeProps, components } from "react-select";
 
 const NoOptionsMessage = (props: NoticeProps) => {
   return (
     <components.NoOptionsMessage {...props}>
-      <span className="">Ничего не нашлось</span>
+      <span className="">
+        Ничего не нашлось.{" "}
+        <Link
+          to="/food/simple"
+          className="underline text-near_black hover:text-light_near_black transition duration-1000 hover:duration-500"
+        >
+          Создать?
+        </Link>
+      </span>
     </components.NoOptionsMessage>
   );
 };
